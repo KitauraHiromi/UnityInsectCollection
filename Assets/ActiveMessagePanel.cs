452eb706bc90ee12d313b6a4d66218f47732bb26
KitauraHiromi <kasumiga_eria394@yahoo.co.jp>
2018-04-21 11:56:22 +0900

    first commit

---
+++ Assets/ActiveMessagePanel.cs
@@ -0,0 +1,50 @@
+﻿    
+using UnityEngine;
+using System.Collections;
+    
+public class ActiveMessagePanel : MonoBehaviour {
+
+    //　MessageUIに設定されているMessageスクリプトを設定
+    [SerializeField]
+    private Message messageScript;
+
+    private GameObject refObj;
+
+    public string cubeTag="Cube";
+    Ray ray;
+    RaycastHit hit = new RaycastHit();
+
+    //　表示させるメッセージ
+    private string message = "かかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかか"
+                             + "ききききききききききききききききききききききききききききききききききききききききききききききききききききききき\n"
+                             + "くくくくく\n"
+                             + "けけけけけけけけけけけけ";
+    
+    void Start() {
+        refObj = GameObject.Find( "MessageUI" );
+        messageScript = refObj.GetComponent<Message>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        // 右クリックで呼び出し
+        if (Input.GetButtonDown ("Fire2")) {
+            messageScript.SetMessagePanel(message);
+        }
+
+        if (    Input.GetMouseButtonDown(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            //マウスクリックした場所からRayを飛ばし、オブジェクトがあればtrue 
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity))
+            {
+                if(hit.collider.gameObject.CompareTag(cubeTag))
+                {
+                    // cubeControlクラスにmessageScriptオブジェクトを渡して，定められたメッセージを表示する.
+                    hit.collider.gameObject.GetComponent<CubeControl>().OnUserAction(messageScript);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Script/CubeControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CubeControl : MonoBehaviour {
+    
+    // Rigidbody rigidBody;
+    // GameObject messagepanel;
+    // public Vector3 force = new Vector3(0, 10, 0);
+    // public ForceMode forceMode = ForceMode.VelocityChange;
+    
+    private string message = "テストメッセージ\n"
+    + "ここに種ごとの説明が入る予定\n";
+    
+    // Use this for initialization
+    void Start () {
+        // rigidBody = gameObject.GetComponent<Rigidbody>();
+    }
+    
+    public void OnUserAction(Message messageScript)
+    {
+        messageScript.SetMessagePanel(message);
+    }
+}
