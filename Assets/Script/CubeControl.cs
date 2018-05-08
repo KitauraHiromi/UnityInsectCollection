d92d62fa9733aa59d3de296bfdeb054f4e2d735d
KitauraHiromi <kasumiga_eria394@yahoo.co.jp>
2018-05-08 20:44:51 +0900

    [add] flapping and roost motion

--- Assets/Script/CubeControl.cs
+++ Assets/Script/CubeControl.cs
@@ -9,8 +9,10 @@ public class CubeControl : MonoBehaviour {
     // public Vector3 force = new Vector3(0, 10, 0);
     // public ForceMode forceMode = ForceMode.VelocityChange;
 
-    float x, y, z;
-    float speed, radius, yPosition;
+    private float x, y, z;
+    private float speed, radius, yPosition;
+    private CharacterController characterController;
+    private Animator animator;
 
     private string message = "テストメッセージ\n"
     + "ここに種ごとの説明が入る予定\n";
@@ -27,24 +29,46 @@ public class CubeControl : MonoBehaviour {
         z = tmp[2] * speed;
     }
 
+    void Roost(){
+        speed = 0f;
+    }
+
     void WalkAround(ref float x, ref float y, ref float z){
         x = radius * Mathf.Sin(Time.time * speed / 2 / radius / Mathf.PI);
         y = 0.0f;
         z = radius * Mathf.Cos (Time.time * speed / 2/ radius / Mathf.PI);
     }
 
+    void SpeedControl(float dist, float thresold){
+        if (dist < thresold){
+            speed = speed * dist / thresold;
+        }
+    }
+
     // Use this for initialization
     void Start () {
         speed = 1.0f;
         radius = 0.3f;
+        characterController = GetComponent <CharacterController> ();
+        animator = GetComponent <Animator> ();
+        animator.SetBool("IsRoost", false);
     }
 
     void Update(){
         // グローバルな移動を実装
         // WalkAround(ref x, ref y, ref z);
-        GoToDestination(new Vector3(0, 0, 0), ref x, ref y, ref z);
+        float dist = Vector3.Distance(this.transform.position, new Vector3(0, 0, 0));
+        if (dist > 0.5){
+            GoToDestination(new Vector3(0, 0, 0), ref x, ref y, ref z);
+            SpeedControl(dist, 1.0f);
+        }
+        else{
+            Roost();
+            animator.SetBool("IsRoost", true);
+        }
         this.transform.rotation = Quaternion.LookRotation(new Vector3(-x, -y, -z));
         this.transform.position += new Vector3(x, y, z);
+
     }
 
 
